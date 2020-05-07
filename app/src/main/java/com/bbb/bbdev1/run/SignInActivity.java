6425c2499a231d9b1f340f5a4b50b717abb0712e
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-05-07 14:03:53 -0700

    Add SignIn Activity and empty Register Activity -Rename MainActivity to SignInActivity -Add Email and Password EditText views and Sign In/Register buttons -Add navigation to Register Activity when clicking Register button -Register Activity still empty

---
+++ app/src/main/java/com/bbb/bbdev1/run/SignInActivity.java
@@ -0,0 +1,24 @@
+package com.bbb.bbdev1.run;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+
+    public void signIn(View view) {
+    }
+
+    public void register(View view) {
+        Intent registerIntent = new Intent(SignInActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+}
