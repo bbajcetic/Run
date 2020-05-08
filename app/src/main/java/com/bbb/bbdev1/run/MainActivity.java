10d1cd1dc9eb331fa0b075ab5916e606d2b3b4f5
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-05-07 23:49:03 -0700

    Add app bar to Main Activity with "Settings" and "Edit Profile" options that currently do nothing

--- app/src/main/java/com/bbb/bbdev1/run/MainActivity.java
+++ app/src/main/java/com/bbb/bbdev1/run/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bbb.bbdev1.run;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
