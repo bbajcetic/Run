0c55870d58756bce07fb5c77207cbdc4378274f2
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-05-07 17:43:20 -0700

    Updated Registration Activity Added all necessary field for the Registration Activity except for the display picture and the change button to change the picture. Also missing the top right Register button in the App Bar

--- app/src/main/java/com/bbb/bbdev1/run/SignInActivity.java
+++ app/src/main/java/com/bbb/bbdev1/run/SignInActivity.java
@@ -6,16 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class SignInActivity extends AppCompatActivity {
+
+    TextInputEditText emailField;
+    TextInputEditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
         setTitle("Sign in");
+
+        emailField = findViewById(R.id.email_field);
+        passwordField = findViewById(R.id.password_field);
     }
 
     public void signIn(View view) {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
     }
 
     public void register(View view) {
