76fe8f13b864626c61519e81c492b32df165a8b5
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-05-07 19:46:58 -0700

    Add custom App Bar to Register Activity with "REGISTER" button -Add a custom appbar and toolbar for Register Activity with one menu item: Register, that for now takes the user back to the Sign In screen -Also fix Up navigation from Register Activity back to Sign In screen (didn't work after adding custom app bar because setDisplayHomeAsUpEnabled(true) was required

--- app/src/main/java/com/bbb/bbdev1/run/RegisterActivity.java
+++ app/src/main/java/com/bbb/bbdev1/run/RegisterActivity.java
@@ -1,8 +1,14 @@
 package com.bbb.bbdev1.run;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,5 +17,27 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //enable Up navigation
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_register) {
+            Intent intent = new Intent(RegisterActivity.this, SignInActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
