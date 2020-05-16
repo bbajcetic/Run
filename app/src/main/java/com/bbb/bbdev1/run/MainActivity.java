69397aca33ce619e90a300fa66e0ddc4697830d6
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-05-15 17:09:58 -0700

    Add floating action button for main action that starts activity recording

--- app/src/main/java/com/bbb/bbdev1/run/MainActivity.java
+++ app/src/main/java/com/bbb/bbdev1/run/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton startFab = findViewById(R.id.start_fab);
+        startFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Start clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
