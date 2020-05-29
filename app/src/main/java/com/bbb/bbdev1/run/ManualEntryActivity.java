c378f40a4917faf6a9cb56fb1b8a1e63efad28e3
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-05-28 20:48:24 -0700

    Connect the onClick event for each Manual Entry Activity attribute
    For each attribute, create a dialog with RunDialogFragment by passing
    in the correct enum value (represents the type of attribute, and
    therefore the type of dialog to display)

--- app/src/main/java/com/bbb/bbdev1/run/ManualEntryActivity.java
+++ app/src/main/java/com/bbb/bbdev1/run/ManualEntryActivity.java
@@ -1,4 +1,5 @@
 package com.bbb.bbdev1.run;
+import com.bbb.bbdev1.run.RunDialogFragment.Type;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,6 +7,7 @@ import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +15,7 @@ import android.widget.Toast;
 import java.sql.Date;
 import java.sql.Time;
 
-public class ManualEntryActivity extends AppCompatActivity implements RunDialogFragment.OnDialogResponseListener {
+public class ManualEntryActivity extends AppCompatActivity implements View.OnClickListener, RunDialogFragment.OnDialogResponseListener {
 
     String activity;
     Date date;
@@ -29,12 +31,14 @@ public class ManualEntryActivity extends AppCompatActivity implements RunDialogF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_entry);
 
-        TextView activity = findViewById(R.id.activity);
-        activity.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showDialog();
-            }
-        });
+        findViewById(R.id.activity).setOnClickListener(this);
+        findViewById(R.id.date).setOnClickListener(this);
+        findViewById(R.id.time).setOnClickListener(this);
+        findViewById(R.id.duration).setOnClickListener(this);
+        findViewById(R.id.distance).setOnClickListener(this);
+        findViewById(R.id.calorie).setOnClickListener(this);
+        findViewById(R.id.heartbeat).setOnClickListener(this);
+        findViewById(R.id.comment).setOnClickListener(this);
 
         if (savedInstanceState != null) {
             date = (Date) savedInstanceState.getSerializable("DATE");
@@ -47,9 +51,39 @@ public class ManualEntryActivity extends AppCompatActivity implements RunDialogF
         }
 
     }
-    void showDialog() {
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.date:
+                showDialog(Type.DATE);
+                break;
+            case R.id.time:
+                showDialog(Type.TIME);
+                break;
+            case R.id.duration:
+                showDialog(Type.DURATION);
+                break;
+            case R.id.distance:
+                showDialog(Type.DISTANCE);
+                break;
+            case R.id.calorie:
+                showDialog(Type.CALORIE);
+                break;
+            case R.id.heartbeat:
+                showDialog(Type.HEARTBEAT);
+                break;
+            case R.id.comment:
+                showDialog(Type.COMMENT);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void showDialog(RunDialogFragment.Type type) {
         DialogFragment newFragment = RunDialogFragment
-                .newInstance(RunDialogFragment.Type.DISTANCE);
+                .newInstance(type);
         newFragment.show(getSupportFragmentManager(), "dialog");
     }
 
