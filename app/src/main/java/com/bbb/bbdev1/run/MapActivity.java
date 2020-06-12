05e1182a75fe1e2939927eec313d175c6f5f3aa1
Branko Bajcetic <bajcetic.branko@gmail.com>
2020-06-12 13:03:15 -0700

    Make MapActivity home button pop Activity off stack instead of regular up/home button functionality (which causes original activity to be recreated). This allows the "Input Type" and "Activity Type" spinners to retain their selected value when the user presses the home/up button from the Map activity instead of resetting to "Manual" and "Running" values on the spinner

--- app/src/main/java/com/bbb/bbdev1/run/MapActivity.java
+++ app/src/main/java/com/bbb/bbdev1/run/MapActivity.java
@@ -1,7 +1,9 @@
 package com.bbb.bbdev1.run;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MapActivity extends AppCompatActivity {
@@ -10,6 +12,16 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
